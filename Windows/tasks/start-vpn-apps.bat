d4f8dac69ab2f27d1fdf0eebf8bc79347591c21e
jesus2099 <jesus2099@users.noreply.github.com>
2021-03-18 00:34:26 +0100

    Scheduled tasks
    - My post-VPN-connect init
    - Fast keyboard as importable XML

---
+++ Windows/tasks/start-vpn-apps.bat
@@ -0,0 +1,10 @@
+@echo off
+
+query process|find /i "outlook.exe">NUL
+if %errorlevel% equ 1 (echo Starting Outlook & start "Outlook" "C:\Program Files (x86)\Microsoft Office\Office16\OUTLOOK.EXE")
+
+query process|find /i "opentouchcon...">NUL
+if %errorlevel% equ 1 (echo Starting OpenTouch Conversation & start "OpenTouch Conversation" "C:\Program Files (x86)\Alcatel-Lucent Enterprise\OpenTouch Conversation\OpenTouchConversation.exe")
+
+query process|find /i "lync.exe">NUL
+if %errorlevel% equ 1 (echo Starting Skype & start "Skype" "C:\Program Files (x86)\Microsoft Office\Office16\lync.exe")
