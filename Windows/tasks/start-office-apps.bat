2579d3939420d3c84f0df52ea1e8bf480a54d30b
jesus2099 <jesus2099@users.noreply.github.com>
2022-05-12 00:26:31 +0200

    Revert non-working solution
    I should use https://stackoverflow.com/a/43804074/2236179
    wmic os get LastBootUpTime,LocalDateTime
    Only works with Fast Startup disabled

--- Windows/tasks/start-office-apps.bat
+++ Windows/tasks/start-office-apps.bat
@@ -1,33 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
 
-if !%~n0-already-started! neq 1 (
+query process|find /i "outlook.exe">nul
+if !errorlevel! equ 1 (echo Starting Outlook & start "Outlook" "%ProgramFiles% (x86)\Microsoft Office\Office16\OUTLOOK.EXE")
 
-	query process|find /i "outlook.exe">nul
-	if !errorlevel! equ 1 (echo Starting Outlook & start "Outlook" "%ProgramFiles% (x86)\Microsoft Office\Office16\OUTLOOK.EXE")
+query process|find /i "opentouchcon...">nul
+if !errorlevel! equ 1 (echo Starting OpenTouch Conversation & start "OpenTouch Conversation" "%ProgramFiles% (x86)\Alcatel-Lucent Enterprise\OpenTouch Conversation\OpenTouchConversation.exe")
 
-	query process|find /i "opentouchcon...">nul
-	if !errorlevel! equ 1 (echo Starting OpenTouch Conversation & start "OpenTouch Conversation" "%ProgramFiles% (x86)\Alcatel-Lucent Enterprise\OpenTouch Conversation\OpenTouchConversation.exe")
-
-	query process|find /i "teams.exe">nul
+query process|find /i "teams.exe">nul
+if !errorlevel! equ 1 (
+	choice /c yn /d y /t 4 /m "Start Teams"
 	if !errorlevel! equ 1 (
-		choice /c yn /d y /t 4 /m "Start Teams"
-		if !errorlevel! equ 1 (
-			echo Starting Teams
-			rem Teams.exe often leaves command window open with some error logs
-			rem start "Teams" "%LocalAppData%\Microsoft\Teams\current\Teams.exe"
-			rem This works good but requires nrcmd.exe (excellent tool)
-			rem %USERPROFILE%\Documents\PRG\DOS\nircmd exec hide "%LocalAppData%\Microsoft\Teams\current\Teams.exe"
-			set _hiddenLauncher=%~dpn0_hiddenLauncher.vbs
-			> "!_hiddenLauncher!" (
-				echo CreateObject^("Wscript.Shell"^).Run """" ^& "%LocalAppData%\Microsoft\Teams\current\Teams.exe" ^& """", 0, False
-			)
-			cscript "!_hiddenLauncher!" //nologo
-			del "!_hiddenLauncher!"
+		echo Starting Teams
+		rem Teams.exe often leaves command window open with some error logs
+		rem start "Teams" "%LocalAppData%\Microsoft\Teams\current\Teams.exe"
+		rem This works good but requires nrcmd.exe (excellent tool)
+		rem %USERPROFILE%\Documents\PRG\DOS\nircmd exec hide "%LocalAppData%\Microsoft\Teams\current\Teams.exe"
+		set _hiddenLauncher=%~dpn0_hiddenLauncher.vbs
+		> "!_hiddenLauncher!" (
+			echo CreateObject^("Wscript.Shell"^).Run """" ^& "%LocalAppData%\Microsoft\Teams\current\Teams.exe" ^& """", 0, False
 		)
+		cscript "!_hiddenLauncher!" //nologo
+		del "!_hiddenLauncher!"
 	)
-
 )
-
-endlocal
-set %~n0-already-started=1
