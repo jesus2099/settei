63bf9fb330939998dc8e63de3ecd447dd7afc3ee
jesus2099 <jesus2099@users.noreply.github.com>
2022-08-18 15:28:45 +0200

    Office is not detected

--- Windows/tasks/test-office-lan.bat
+++ Windows/tasks/test-office-lan.bat
@@ -1,15 +1,45 @@
 @echo off
-setlocal
+setlocal enableDelayedExpansion
 
 del %~dp0_vpn-flag
 
+(
+	echo.
+	echo ----------
+	echo %~nx0 version 2022-08-18 version %~t0
+	echo.
+	date /t
+	time /t
+) >> %~dpn0.log
+
+rem -----------------------------------------------------------------------
+rem https://ss64.com/nt/findstr.html
+rem https://ss64.com/nt/findstr-linebreaks.html Searching across Line Breaks: CR and LF characters must be matched explictly
+rem https://ss64.com/nt/findstr-escapes.html Search String length limits: 511 bytes for text / 254 bytes for regular expressions (actually 278 bytes on my PC)
+rem Define LF variable containing a linefeed (0x0A)
+set LF=^
+
+
+rem Above 2 blank lines are critical - do not remove NICHT
+rem Define CR variable containing a carriage return (0x0D)
+for /f %%a in ('copy /z "%~dpf0" nul') do set "CR=%%a"
+rem -----------------------------------------------------------------------
+
 rem Code Page 850
-                      ipconfig|find "Suffixe DNS propre … la connexion. . . : numericable.fr"
-if %errorlevel% equ 0 ipconfig|find "Adresse IPv6 de liaison locale. . . . .: fe80::"
-if %errorlevel% equ 0 ipconfig|find "Adresse IPv4. . . . . . . . . . . . . .: 192.168.0."
-if %errorlevel% equ 0 ipconfig|find "Masque de sous-r‚seau. . . .ÿ. . . . . : 255.255.255.0"
-if %errorlevel% equ 0 ipconfig|find "Passerelle par d‚faut. . . .ÿ. . . . . : 192.168.0.1"
+ipconfig | findstr /rc:"^   Suffixe DNS propre … la connexion.*: numericable\.fr!CR!*!LF!   Adresse IPv6 de liaison locale.*: fe80::.*!CR!*!LF!   Adresse IPv4.*: 192\.168\.0\..*!CR!*!LF!   Masque de sous-r‚seau.*: 255\.255\.255\.0!CR!*!LF!   Passerelle par d‚faut.*: 192\.168\.0\.1$" >> %~dpn0.log
 set _home=%errorlevel%
+
+rem Code Page 850
+ipconfig | findstr /rc:"^   Suffixe DNS propre … la connexion.*: fr.*\.ad\..*ance\.fr!CR!!LF!   Adresse IPv6 de liaison locale.*: fe80::" >> %~dpn0.log
+set _office=%errorlevel%
+
+set _location=other
+if %_home%%_office% == 01 set _location=home
+if %_home%%_office% == 10 set _location=office
+
+goto %_location%
+
+:other
 
 ping github.com
 set _github=%errorlevel%
@@ -20,22 +50,9 @@ set _gmail=%errorlevel%
 ping ymail.com
 set _ymail=%errorlevel%
 
-set _location=other
+set _location=offline
+if %_github%%_gmail%%_ymail% not 111 set _location=online
 
-if %_home%%_github%%_gmail%%_ymail% == 0000 set _location=home
-if %_home%%_github%%_gmail%%_ymail% == 1011 set _location=office
-if %_home%%_github%%_gmail%%_ymail% == 1111 set _location=offline
-
-(
-	echo ----------
-	echo.
-	date /t
-	time /t
-	echo %_location% ^(home, github.com, gmail.com, ymail.com: %_home%%_github%%_gmail%%_ymail%^)
-	echo.
-) >>%~dpn0.log
-
-echo %_location%
 goto %_location%
 
 :office
@@ -48,7 +65,7 @@ goto end
 query process|find /i "move mouse.exe">NUL
 if %errorlevel% equ 1 (echo Starting Move Mouse ^(anti lock^) & start "Move Mouse" shell:appsfolder\1258EllAbi.MoveMouse_hjfwaxvfbwh7t!MyDesktopAppStep1)
 
-:other
+:online
 
 start "PingID" "%ProgramFiles% (x86)\Ping Identity\PingID\PingID.exe"
 goto end
@@ -56,3 +73,6 @@ goto end
 :offline
 
 :end
+
+echo location: %_location% >> %~dpn0.log
+echo errorlevels ^(home, office, github.com, gmail.com, ymail.com^): %_home%.%_office%.%_github%.%_gmail%.%_ymail% >> %~dpn0.log
