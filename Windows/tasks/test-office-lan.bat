7e88b441c2fbf424af0c5e056fb2d957cc5ac3cd
jesus2099 <jesus2099@users.noreply.github.com>
2023-04-20 13:01:09 +0200

    Enforce character code page 850 (maybe useless)

--- Windows/tasks/test-office-lan.bat
+++ Windows/tasks/test-office-lan.bat
@@ -5,7 +5,7 @@ del %~dp0_vpn-flag
 
 (
 	echo.
-	echo ----------
+	echo #######################################################################
 	echo %~nx0 version %~t0
 	echo.
 	date /t
@@ -25,11 +25,11 @@ rem Define CR variable containing a carriage return (0x0D)
 for /f %%a in ('copy /z "%~dpf0" nul') do set "CR=%%a"
 rem -----------------------------------------------------------------------
 
-rem Code Page 850
+mode con cp select=850
+
 ipconfig | findstr /rc:"^   Suffixe DNS propre … la connexion.*: numericable\.fr!CR!*!LF!   Adresse IPv6 de liaison locale.*: fe80::.*!CR!*!LF!   Adresse IPv4.*: 192\.168\.0\..*!CR!*!LF!   Masque de sous-r‚seau.*: 255\.255\.255\.0!CR!*!LF!   Passerelle par d‚faut.*: 192\.168\.0\.1$" >> %~dpn0.log
 set _home=%errorlevel%
 
-rem Code Page 850
 for /f "tokens=11 delims= " %%a in ('ipconfig ^| findstr /nrc:"Suffixe DNS propre … la connexion[. ]*: fr.*\.ad\.[fiancer]*\.fr$"') do set _dns=%%a
 
 if defined _dns (
