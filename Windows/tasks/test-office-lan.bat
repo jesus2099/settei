c5b27bead5b4f4104de28f31c9900182dc796871
jesus2099 <jesus2099@users.noreply.github.com>
2023-04-21 09:46:51 +0200

    Hide codepage change output, log errors

--- Windows/tasks/test-office-lan.bat
+++ Windows/tasks/test-office-lan.bat
@@ -25,7 +25,7 @@ rem Define CR variable containing a carriage return (0x0D)
 for /f %%a in ('copy /z "%~dpf0" nul') do set "CR=%%a"
 rem -----------------------------------------------------------------------
 
-mode con cp select=850
+mode con cp select=850 > nul 2>> %~dpn0.log
 
 ipconfig | findstr /rc:"^   Suffixe DNS propre … la connexion.*: numericable\.fr!CR!*!LF!   Adresse IPv6 de liaison locale.*: fe80::.*!CR!*!LF!   Adresse IPv4.*: 192\.168\.0\..*!CR!*!LF!   Masque de sous-r‚seau.*: 255\.255\.255\.0!CR!*!LF!   Passerelle par d‚faut.*: 192\.168\.0\.1$" >> %~dpn0.log
 set _home=%errorlevel%
