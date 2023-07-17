b6bbfa9ff332b88cf6b4db80fd91351534e2cb85
jesus2099 <jesus2099@users.noreply.github.com>
2023-07-17 11:34:34 +0200

    compression level: ultra

--- MS-DOS/zip_to_7z.bat
+++ MS-DOS/zip_to_7z.bat
@@ -3,7 +3,7 @@ rem edit of https://superuser.com/a/645821/387650
 for %%F in (*.zip) do (
 	"%ProgramFiles%\7-Zip\7z.exe" x -y -o"%%F_tmp" "%%F" *
 	pushd "%%F_tmp"
-	"%ProgramFiles%\7-Zip\7z.exe" a -y -r -t7z "..\%%~nF.7z" *
+	"%ProgramFiles%\7-Zip\7z.exe" a -y -r -t7z -mx9 "..\%%~nF.7z" *
 	popd
 	rmdir /s /q "%%F_tmp"
 )
