d783800d90d9c8da212ad6fe2a2f6ff5f67156d9
jesus2099 <jesus2099@users.noreply.github.com>
2023-07-17 11:29:29 +0200

    layout

--- MS-DOS/zip_to_7z.bat
+++ MS-DOS/zip_to_7z.bat
@@ -1,3 +1,9 @@
 @echo off
-rem https://superuser.com/a/645821/387650
-for %%F in (*.zip) do ( "C:\Program Files\7-Zip\7z.exe" x -y -o"%%F_tmp" "%%F" * & pushd %%F_tmp & "C:\Program Files\7-Zip\7z.exe" a -y -r -t7z ..\"%%~nF".7z * & popd & rmdir /s /q "%%F_tmp" )
+rem edit of https://superuser.com/a/645821/387650
+for %%F in (*.zip) do (
+	"C:\Program Files\7-Zip\7z.exe" x -y -o"%%F_tmp" "%%F" *
+	pushd %%F_tmp
+	"C:\Program Files\7-Zip\7z.exe" a -y -r -t7z ..\"%%~nF".7z *
+	popd
+	rmdir /s /q "%%F_tmp"
+)
