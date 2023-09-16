51e96951c7fe15775749b907138ea6b1e0990a15
jesus2099 <jesus2099@users.noreply.github.com>
2023-09-16 12:16:34 +0200

    Locate Vivaldi and 32/64-bit Outlook

--- Windows/tasks/start-office-apps.bat
+++ Windows/tasks/start-office-apps.bat
@@ -3,15 +3,37 @@ setlocal enabledelayedexpansion
 
 if not exist "%~dp0_vpn-flag" (
 
-	query process | find /i "vivaldi.exe">nul
-	if !errorlevel! equ 1 (echo Starting Vivaldi... & start "Vivaldi" "%LocalAppData%\Programs\Vivaldi\Application\vivaldi.exe")
+	query process | find /i "vivaldi.exe" >nul
+	if !errorlevel! equ 1 (
+		for %%V in (
+			"%LocalAppData%\Vivaldi\Application\vivaldi.exe"
+			"%LocalAppData%\Programs\Vivaldi\Application\vivaldi.exe"
+		) do if exist %%V (
+			start "Vivaldi" %%V
+			goto :vivaldi_found
+		)
+		echo Vivaldi not found
+		:vivaldi_found
+		echo Starting Vivaldi...
+	)
 
-	query process | find /i "outlook.exe">nul
-	if !errorlevel! equ 1 (echo Starting Outlook... & start "Outlook" "%ProgramFiles%\Microsoft Office\root\Office16\OUTLOOK.EXE")
+	query process | find /i "outlook.exe" >nul
+	if !errorlevel! equ 1 (
+		for %%O in (
+			"%ProgramFiles%\Microsoft Office\root\Office16\OUTLOOK.EXE"
+			"%ProgramFiles(x86)%\Microsoft Office\Office16\OUTLOOK.EXE"
+		) do if exist %%O (
+			start "Outlook" %%O
+			goto :outlook_found
+		)
+		echo Outlook not found
+		:outlook_found
+		echo Starting Outlook...
+	)
 
 	if exist "%~dp0_reachable-flag" (
 
-		query process | find /i "teams.exe">nul
+		query process | find /i "teams.exe" >nul
 		if !errorlevel! equ 1 (
 			echo Starting Teams...
 			rem Teams.exe often leaves command window open with some error logs
@@ -26,7 +48,7 @@ if not exist "%~dp0_vpn-flag" (
 			del "!_hiddenLauncher!"
 		)
 
-		query process | find /i "opentouchcon...">nul
+		query process | find /i "opentouchcon..." >nul
 		if !errorlevel! equ 1 (
 			echo Starting OpenTouch Conversation...
 			start "OpenTouch Conversation" "%ProgramFiles% (x86)\Alcatel-Lucent Enterprise\OpenTouch Conversation\OpenTouchConversation.exe"
