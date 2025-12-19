911104250f9ac1fc640f2bafeb2dbfd7dc336253
jesus2099 <jesus2099@users.noreply.github.com>
2025-12-19 17:06:42 +0100

    fix: Teams not started from office

--- Windows/tasks/test-office-lan.bat
+++ Windows/tasks/test-office-lan.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-del "%~dp0_vpn-flag"
-del "%~dp0_reachable-flag"
+del "%~dp0_vpn-flag" 2>nul
+del "%~dp0_reachable-flag" 2>nul
 
 (
 	echo.
@@ -51,6 +51,7 @@ set _location=other
 if !_home!!_office! == 01 set _location=home
 if !_home!!_office! == 10 set _location=office
 
+echo ### !_location! ###
 goto !_location!
 
 :other
@@ -67,44 +68,18 @@ set _ymail=!errorlevel!
 set _location=offline
 if not !_github!!_gmail!!_ymail! == 111 set _location=online
 
-echo goto !_location!
+echo ### !_location! ###
 goto !_location!
 
 :office
 
 call "%~dp0\start-office-apps.bat"
-goto end
+goto online
 
 :home
 
 echo Starting Caffeine on ^(keep PC awake^)...
 start "Caffeine" "%LocalAppData%\Programs\_\caffeine64.exe" -stes -onac -notwhenlocked -replace
-
-for /f "skip=1" %%a in ('wmic Path Win32_Battery Get BatteryStatus') do for %%b in (2 6 7 8) do if %%a == %%b goto power
-
-rem my laptop only knows statuses 1 for battery and 2 for power cord
-rem https://learn.microsoft.com/windows/win32/cimwin32prov/win32-battery
-rem Other (1) The battery is discharging.
-rem Unknown (2) The system has access to AC so no battery is being discharged. However, the battery is not necessarily charging.
-rem Fully Charged (3)
-rem Low (4)
-rem Critical (5)
-rem Charging (6)
-rem Charging and High (7)
-rem Charging and Low (8)
-rem Charging and Critical (9)
-rem Undefined (10)
-rem Partially Charged (11)
-
-:battery
-
-echo On battery
-
-goto online
-
-:power
-
-echo Power connected
 
 :online
 
@@ -120,6 +95,39 @@ if !_location! equ office (
 	echo.
 	choice /c ync /n /d y /t 10 /m "[Yes] / No / Connected but not reachable"
 	set _vpn=!errorlevel!
+
+	if !_vpn! neq 2 (
+		echo Starting PingID...
+		start "PingID" "%ProgramFiles(x86)%\Ping Identity\PingID\PingID.exe"
+		echo Starting Pulse Secure...
+		start "Pulse Secure" "%ProgramFiles(x86)%\Common Files\Pulse Secure\JamUI\Pulse.exe" -show
+		where wevtutil >nul 2>nul && where timeout >nul 2>nul && where powershell >nul 2>nul
+		if !errorlevel! equ 0 (
+			set _vpn_wait_time_min=30
+			set _vpn_wait_check_delay_s=2
+			set /a "_vpn_wait_time_s= _vpn_wait_time_min * 60"
+			set /a "_vpn_max_retry= _vpn_wait_time_s / _vpn_wait_check_delay_s"
+			for /f "delims=" %%T in ('powershell get-date -format "yyyy-MM-ddTHH:mm:ssZ" ^(get-date^).toUniversalTime^(^)') do set _wait_start_ts=%%T
+			set /p "=Waiting for VPN ^(!_vpn_wait_time_min! min max^).." <nul
+			:wait_for_vpn
+			set /a "_vpn_max_retry-=1"
+			set /p "=." <nul
+			if !_vpn_max_retry! leq 0 (
+				echo.
+				goto stop_wait_for_vpn
+			)
+			wevtutil qe /rd:true /f:text /q:"Event[System[Provider[@Name='IVE']][EventID=312][TimeCreated[@SystemTime ^>= '!_wait_start_ts!']]]" "Pulse Secure/Operational"|find "was established successfully to https://"
+			if !errorlevel! equ 0 (
+				call "%~dp0\start-office-apps.bat"
+				goto stop_wait_for_vpn
+			) else (
+				timeout /t !_vpn_wait_check_delay_s! >nul
+				goto wait_for_vpn
+			)
+			:stop_wait_for_vpn
+			rem no empty line after label
+		)
+	)
 )
 
 if !_vpn! equ 1 (
@@ -129,50 +137,36 @@ if !_vpn! equ 1 (
 		echo Starting Teams...
 		rem find app ID by exploring shell:AppsFolder and create shortcut
 		start shell:AppsFolder\MSTeams_8wekyb3d8bbwe^^!MSTeams
+	) else (
+		echo Teams is running
 	)
 )
 
-if !_vpn! neq 2 (
-	echo Starting PingID...
-	start "PingID" "%ProgramFiles(x86)%\Ping Identity\PingID\PingID.exe"
-	echo Starting Pulse Secure...
-	start "Pulse Secure" "%ProgramFiles(x86)%\Common Files\Pulse Secure\JamUI\Pulse.exe" -show
-	where wevtutil >nul 2>nul && where timeout >nul 2>nul && where powershell >nul 2>nul
-	if !errorlevel! equ 0 (
-		set _vpn_wait_time_min=30
-		set _vpn_wait_check_delay_s=2
-		set /a "_vpn_wait_time_s= _vpn_wait_time_min * 60"
-		set /a "_vpn_max_retry= _vpn_wait_time_s / _vpn_wait_check_delay_s"
-		for /f "delims=" %%T in ('powershell get-date -format "yyyy-MM-ddTHH:mm:ssZ" ^(get-date^).toUniversalTime^(^)') do set _wait_start_ts=%%T
-		set /p "=Waiting for VPN ^(!_vpn_wait_time_min! min max^).." <nul
-		:wait_for_vpn
-		set /a "_vpn_max_retry-=1"
-		set /p "=." <nul
-		if !_vpn_max_retry! leq 0 (
-			echo.
-			goto stop_wait_for_vpn
-		)
-		wevtutil qe /rd:true /f:text /q:"Event[System[Provider[@Name='IVE']][EventID=312][TimeCreated[@SystemTime ^>= '!_wait_start_ts!']]]" "Pulse Secure/Operational"|find "was established successfully to https://"
-		if !errorlevel! equ 0 (
-			call "%~dp0\start-office-apps.bat"
-			goto stop_wait_for_vpn
-		) else (
-			timeout /t !_vpn_wait_check_delay_s! >nul
-			goto wait_for_vpn
-		)
-		:stop_wait_for_vpn
-		rem echo prout
-	)
-)
-echo goto end
 goto end
 
 :offline
 
 :end
 
-echo location: !_location!
-echo location: !_location! >>"%~dpn0.log"
+echo Location: !_location!
+echo Location: !_location! >>"%~dpn0.log"
 echo errorlevels ^(home, office, github.com, gmail.com, ymail.com^): !_home!.!_office!.!_github!.!_gmail!.!_ymail! >>"%~dpn0.log"
 
-timeout /t 10
+set _power=battery
+for /f "skip=1" %%a in ('wmic Path Win32_Battery Get BatteryStatus') do for %%b in (2 6 7 8) do if %%a == %%b set _power=connected
+rem my laptop only knows statuses 1 for battery and 2 for power cord
+rem https://learn.microsoft.com/windows/win32/cimwin32prov/win32-battery
+rem Other (1) The battery is discharging.
+rem Unknown (2) The system has access to AC so no battery is being discharged. However, the battery is not necessarily charging.
+rem Fully Charged (3)
+rem Low (4)
+rem Critical (5)
+rem Charging (6)
+rem Charging and High (7)
+rem Charging and Low (8)
+rem Charging and Critical (9)
+rem Undefined (10)
+rem Partially Charged (11)
+echo Power: !_power!
+
+timeout /t 20
